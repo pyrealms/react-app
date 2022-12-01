f5567c7c0ddfa76a845875685953989c8fe8c628
Soren Olsen <soren@celavier.com>
2022-12-01 01:00:06 -0500

    Testing options from file

--- .github/actions/secrets/src/index.js
+++ .github/actions/secrets/src/index.js
@@ -1,10 +1,9 @@
 const sodium = require("libsodium-wrappers");
 const core = require("@actions/core");
 
-//const text = core.getInput("text"); // replace with the secret you want to encrypt
+const text = core.getInput("text"); // replace with the secret you want to encrypt
 //const key = core.getInput("key"); // replace with the Base64 encoded public key
 
-const text = "Some Text To Encrypt"; // replace with the secret you want to encrypt
 const key = "HkXSTjnwkF4HZZ9mx9BAJ9UxeSe38yP0gvpZuc8iITQ="; // replace with the Base64 encoded public key
 
 console.log("Text: ", text);
