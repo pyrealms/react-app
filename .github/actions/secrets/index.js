b47bdd5cf6ab48ddf8a2b3a51e832917dae2bf8c
Soren Olsen <soren@celavier.com>
2022-12-01 01:09:04 -0500

    Testing options from file

--- .github/actions/secrets/index.js
+++ .github/actions/secrets/index.js
@@ -2836,9 +2836,9 @@ const sodium = __webpack_require__(726);
 const core = __webpack_require__(470);
 
 const text = core.getInput("text"); // replace with the secret you want to encrypt
-//const key = core.getInput("key"); // replace with the Base64 encoded public key
+const key = core.getInput("key"); // replace with the Base64 encoded public key
 
-const key = "HkXSTjnwkF4HZZ9mx9BAJ9UxeSe38yP0gvpZuc8iITQ="; // replace with the Base64 encoded public key
+//const key = "HkXSTjnwkF4HZZ9mx9BAJ9UxeSe38yP0gvpZuc8iITQ="; // replace with the Base64 encoded public key
 
 console.log("Text: ", text);
 console.log("Key: ", key);
--- .github/actions/secrets/src/index.js
+++ .github/actions/secrets/src/index.js
@@ -2,9 +2,9 @@ const sodium = require("libsodium-wrappers");
 const core = require("@actions/core");
 
 const text = core.getInput("text"); // replace with the secret you want to encrypt
-//const key = core.getInput("key"); // replace with the Base64 encoded public key
+const key = core.getInput("key"); // replace with the Base64 encoded public key
 
-const key = "HkXSTjnwkF4HZZ9mx9BAJ9UxeSe38yP0gvpZuc8iITQ="; // replace with the Base64 encoded public key
+//const key = "HkXSTjnwkF4HZZ9mx9BAJ9UxeSe38yP0gvpZuc8iITQ="; // replace with the Base64 encoded public key
 
 console.log("Text: ", text);
 console.log("Key: ", key);
