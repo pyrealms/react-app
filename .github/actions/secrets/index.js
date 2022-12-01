46945ab1b50156ae47aadd4aed13ab56dc695227
Soren Olsen <soren@celavier.com>
2022-12-01 00:57:46 -0500

    Testing options from file

--- .github/actions/secrets/index.js
+++ .github/actions/secrets/index.js
@@ -2835,8 +2835,12 @@ exports.default = _default;
 const sodium = __webpack_require__(726);
 const core = __webpack_require__(470);
 
-const text = core.getInput("text"); // replace with the secret you want to encrypt
-const key = core.getInput("key"); // replace with the Base64 encoded public key
+//const text = core.getInput("text"); // replace with the secret you want to encrypt
+//const key = core.getInput("key"); // replace with the Base64 encoded public key
+
+const text = "Some Text To Encrypt"; // replace with the secret you want to encrypt
+const key = "HkXSTjnwkF4HZZ9mx9BAJ9UxeSe38yP0gvpZuc8iITQ="; // replace with the Base64 encoded public key
+
 console.log("Text: ", text);
 console.log("Key: ", key);
 //Check if libsodium is ready and then proceed.
